c8d1006a5c28168100ef79d37132f788b7f3a5bd
Dripzoid <dripzoidapp@gmail.com>
2025-09-06 21:07:50 +0530

    Delete backend directory

--- backend/migrations/add_product_columns.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE products ADD COLUMN subcategory TEXT;
-ALTER TABLE products ADD COLUMN stock INTEGER DEFAULT 0;
-ALTER TABLE products ADD COLUMN images TEXT; -- JSON string of image URLs
-ALTER TABLE products ADD COLUMN updated_at TEXT;
