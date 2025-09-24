957f73799505225d32fa3473892a3c48b5980c52
Dripzoid <dripzoidapp@gmail.com>
2025-09-24 09:51:24 +0530

    Create LogoLoader.jsx

---
+++ src/components/LogoLoader.jsx
@@ -0,0 +1,9 @@
+// src/components/LogoLoader.jsx
+import React from "react";
+import "./LogoLoader.css";
+
+const LogoLoader = () => {
+  return <div className="logo-loader"></div>;
+};
+
+export default LogoLoader;
